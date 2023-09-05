9ac22547122a734a321c0b745553075984b1cf28
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-05 06:19:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ def get_fruityvice_data(cur_fruit_choice):
   fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + cur_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-back_from_function = get_fruityvice_data('banana')
 
 sl.header("Fruityvice Fruit Advice!")
 try:
@@ -37,7 +36,7 @@ try:
   if not fruit_choice:
     sl.error('Please select a fruit to get information.')
   else:
-    back_from_function = get_fruityvice_data(choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     sl.dataframe(back_from_function)
 except URLError as e: 
   sl.error()
