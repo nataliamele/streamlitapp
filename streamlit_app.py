366cf23540e1b7920b0d772ec92927405b65ee12
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-04 20:04:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as sl
 
 sl.title('This is just a begining of the Streamlit new cool App')
-sl.header('Healthy choices')
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado & Toast')
