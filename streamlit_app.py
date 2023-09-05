924c54a97fe67708b4be0ab36cdc8efdbd305daf
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-04 20:08:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as sl
+import pandas as pd
 
 sl.title('This is just a begining of the Streamlit new cool App')
 
@@ -11,3 +12,5 @@ sl.text('🥑🍞 Avocado & Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.dataframe(my_fruit_list)
