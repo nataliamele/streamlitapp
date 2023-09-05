eb41b715f5f793a02ba81d5f994fa3307e7a4041
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-04 20:26:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Pick list
-sl.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+sl.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 sl.dataframe(my_fruit_list)
