89cee0398a6ab87a27043f2c6c68cd5c31a38ea8
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-04 20:13:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,10 @@ sl.text('🥑🍞 Avocado & Toast')
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#set index to the specific col
+my_fruit_list.my_fruit_list.set_index('Fruit')
+
+# Pick list
+sl.multiselect("Pick some fruits: ", ;list(my_fruit_list.index))
+
 sl.dataframe(my_fruit_list)
