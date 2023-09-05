f5f36217a4410e3a4ef96e5fd6dd91f09b4c7a06
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-04 19:58:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as sl
 
 sl.title('This is just a begining of the Streamlit new cool App')
 sl.header('Healthy choices')
+
+sl.text('Long description text with variety of choices which are benefitial for your health and mood')
