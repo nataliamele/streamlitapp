74615992b90f1028d128316fea9529390e967b5e
Natalia Meleshkina <41652627+nataliamele@users.noreply.github.com>
2023-09-04 23:55:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 sl.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
+sl.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 sl.text(fruityvice_response.json())
